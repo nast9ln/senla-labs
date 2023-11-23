3abeb41f47dc10d5c3a60d6fc44433de7bfdba7f
nastya <nast9ln@vk.com>
2023-11-23 18:26:23 +0300

    запускается coffer

---
+++ src/main/java/org/example/servlet/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorld extends  HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // super.doGet(req, resp);
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hi");
+        printWriter.close();
+    }
+}
